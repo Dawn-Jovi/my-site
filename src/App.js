df9d7d250cf8e8adfa2c9d19397a9c8bfbb5d318
Dawn-Jovi <dawnjovi@163.com>
2025-04-12 22:21:51 +0800

    12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'; // 导入 Navigate 组件
+import { BrowserRouter } from 'react-router-dom';
 import { useEffect } from 'react';
 import Home from './Page-home';
 import About from './Page-about';
