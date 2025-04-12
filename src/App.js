537c57a814f4f7477001e3138cff071ee058d811
Dawn-Jovi <dawnjovi@163.com>
2025-04-12 23:34:25 +0800

    000

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // 导入 Navigate 组件
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import Home from './Page-home';
 import About from './Page-about';
@@ -8,34 +8,12 @@ function App() {
   return (
     <BrowserRouter basename="/my-site">
       <Routes>
-            {/* 根路径重定向到 /home */}
-            <Route path="/" element={<Navigate  to="/home" replace />} />
-            
-            {/* 正常的路由配置 */}
-            <Route
-              path="/home"
-              element={
-                <PageWrapper
-                  title="代码进化录 - 全栈萌新的成长之路"
-                  children={<Home />}
-                />}
-            />
-
-            <Route 
-              path="/about" 
-              element={
-                <PageWrapper title="代码之外"
-                  children={<About/>} 
-              />}
-            />
-            
-            <Route 
-              path="/buildpage" 
-                element={<PageWrapper title="施工中 - 萌新工程师进化ing" children={<BuildPage/>} />}
-            />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<PageWrapper title="首页" children={<Home />} />} />
+        <Route path="/about" element={<PageWrapper title="关于" children={<About />} />} />
+        <Route path="/buildpage" element={<PageWrapper title="施工中" children={<BuildPage />} />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
@@ -43,7 +21,6 @@ function PageWrapper({ title, children }) {
   useEffect(() => {
     document.title = title;
   }, [title]);
-
   return children;
 }
 
