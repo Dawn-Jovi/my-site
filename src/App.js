77aeefb9c3cee6883cde7225d6095d86b8e9ab03
Dawn-Jovi <dawnjovi@163.com>
2025-04-13 12:23:14 +0800

    z-index

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import BuildPage from './Page-build';
 
 function App() {
   return (
-    <HashRouter basename="/my-site">
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="home" replace />} />
         <Route path="/home" element={<PageWrapper title="首页" children={<Home />} />} />
         <Route path="/about" element={<PageWrapper title="关于" children={<About />} />} />
         <Route path="/buildpage" element={<PageWrapper title="施工中" children={<BuildPage />} />} />
